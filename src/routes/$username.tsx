cc94afdc332377243255216f20f364f6e6b9cc57
Clayton Ingalls <github@zapbampow.com>
2023-07-24 08:44:09 -0400

    adds date picker styles

--- src/routes/$username.tsx
+++ src/routes/$username.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunction } from "react-router-dom";
 import { Outlet, redirect } from "react-router-dom";
 import "~/styles/bggStats/username.css";
+import "~/styles/bggStats/datePickerStyles.css";
 
 // Until other features are built, this route simply redirects to $username/plays
 export const loader: LoaderFunction = async ({ params }) => {
