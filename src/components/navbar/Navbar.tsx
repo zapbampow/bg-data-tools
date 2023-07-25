b601e755b607346dcd604ebcdbf2f4e6a1804c0e
Clayton Ingalls <github@zapbampow.com>
2023-07-25 07:34:32 -0400

    updates the url for the home link in the navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,10 @@
 import NavMenu from "./NavMenu";
 import { HomeMeeple } from "../icons";
-import { useParams } from "react-router-dom";
 
 export default function Navbar() {
-  const { username } = useParams();
-
   return (
     <nav className="flex items-center justify-between px-2 py-4 lg:px-8">
-      <a href="/bggstats" className="flex text-slate-100">
+      <a href="/" className="flex text-slate-100">
         <HomeMeeple width={24} />
       </a>
       {/* <div className="flex items-center text-xl font-medium text-slate-100">
