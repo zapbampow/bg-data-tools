488d366568508355c64a19ec353fd74760f7f80a
Clayton Ingalls <github@zapbampow.com>
2023-08-02 12:34:13 -0400

    fix(navbar): uses react router link for the home link

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import NavMenu from "./NavMenu";
 import { HomeMeeple } from "../icons";
 
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between px-2 py-4 lg:px-8">
-      <a href="/" className="flex text-slate-100">
+      <Link to="/" className="flex text-slate-100">
         <HomeMeeple width={24} />
-      </a>
+      </Link>
       {/* <div className="flex items-center text-xl font-medium text-slate-100">
         {username && `Play data for ${username}`}
       </div> */}
