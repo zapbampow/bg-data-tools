5e8dba59b1da4442d13d6cc24a83e53eb85e61ae
Clayton Ingalls <github@zapbampow.com>
2024-08-23 17:11:29 -0400

    removes config related to gh-pages"

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [
     react(),
     topLevelAwait({
