e207abbbb03392d6cb044fa1f1c5daea894ae01c
Clayton Ingalls <github@zapbampow.com>
2024-08-24 07:42:10 -0400

    updates some react imports

--- src/contexts/playFilterContext.tsx
+++ src/contexts/playFilterContext.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import type { FilterType } from "~/services/queryService/types";
 
 type PlayFilterProviderProps = { children: React.ReactNode };
--- src/contexts/userContext/userContext.tsx
+++ src/contexts/userContext/userContext.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { UserInfo } from "~/models/bgg/userInfo.ts";
 
 type UserProviderProps = { children: React.ReactNode };
