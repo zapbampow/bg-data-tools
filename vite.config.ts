12f53f2a4b61cd884c861a008ca39dba12ffe4e0
Clayton Ingalls <github@zapbampow.com>
2024-09-02 09:37:16 -0400

    build: replaces unnecessary await package with an updated build target in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
 import { defineConfig } from "vite";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    topLevelAwait({
-      // The export name of top-level await promise for each chunk module
-      promiseExportName: "__tla",
-      // The function to generate import names of top-level await promise in each chunk module
-      promiseImportName: (i) => `__tla_${i}`,
-    }),
   ],
+  build: {
+    target: "esnext",
+  },
   resolve: {
     alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }],
   },
